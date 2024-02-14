6e8eb9ffff62c1927eac95aef96b9afe28fff3e0
SneakyPotts <4udaksaha@gmail.com>
2024-02-14 12:49:58 +0200

    fix font-family;

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,18 +2,10 @@ import classNames from 'classnames'
 import type { Metadata } from 'next'
 import type { ReactNode } from 'react'
 
-import { Inter, Roboto } from 'next/font/google'
-
+import RobotoFont from '@assets/font'
 import '@assets/scss/main.scss'
 
 import { AppProvider } from '@components/Context/context'
-
-const inter = Inter({ subsets: ['latin'] })
-
-const roboto = Roboto({
-  weight: '400',
-  subsets: ['latin'],
-})
 
 export const metadata: Metadata = {
   title: 'Unity - Courses',
@@ -27,7 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="uk">
-      <body className={classNames(roboto.className, 'body-mob-fixed grid-layout')}>
+      <body className={classNames(RobotoFont.className, 'body-mob-fixed grid-layout')}>
         <AppProvider>{children}</AppProvider>
       </body>
     </html>
