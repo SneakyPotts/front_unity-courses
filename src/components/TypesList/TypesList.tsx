39071dd086da2166744a3e0b622efafbd4827b13
SneakyPotts <4udaksaha@gmail.com>
2024-05-04 19:33:38 +0300

    lesson icons;

--- src/components/TypesList/TypesList.tsx
+++ src/components/TypesList/TypesList.tsx
@@ -5,10 +5,10 @@ import type { TypesListProps } from './TypesList.props'
 
 const typesItems = [
   {
-    id: 'has_online_link',
+    id: 'online_lesson_link',
     tipText: 'Онлайн-урок',
     icon: 'webcam',
-    isCompleted: true,
+    isCompleted: false,
   },
   {
     id: 'has_homework',
@@ -23,8 +23,8 @@ const typesItems = [
     isCompleted: false,
   },
   {
-    id: 'additional_works',
-    tipText: 'Контрольна робота',
+    id: 'has_self_education_work',
+    tipText: 'Самостійна робота',
     icon: 'pen',
     isCompleted: false,
   },
@@ -54,11 +54,11 @@ export function TypesList({ className, data }: TypesListProps) {
       </ul>
 
       {/* need hidden check icon when all tasks completed. Example all test or home work.*/}
-      <div className="types__final">
-        <svg className="types__icon-svg">
-          <use href="/img/sprite.svg#check"></use>
-        </svg>
-      </div>
+      {/*<div className="types__final">*/}
+      {/*  <svg className="types__icon-svg">*/}
+      {/*    <use href="/img/sprite.svg#check"></use>*/}
+      {/*  </svg>*/}
+      {/*</div>*/}
     </div>
   )
 }
